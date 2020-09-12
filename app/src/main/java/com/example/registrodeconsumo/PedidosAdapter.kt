1064d0993e10888b1c67043beba548701d906d9c
katthy <you@example.com>
2020-09-11 22:56:04 -0300

    Entity creada Dao Creado Database Creada Repositorio empezando el ViewModel ViewModel terminando itemlist adapter first fragment

--- app/src/main/java/com/example/registrodeconsumo/PedidosAdapter.kt
+++ app/src/main/java/com/example/registrodeconsumo/PedidosAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.registrodeconsumo
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.registrodeconsumo.database.Pedidos
 import kotlinx.android.synthetic.main.pedidos_item_list.view.*
 
-class PedidosAdapter(val list:List<Pedidos>) : RecyclerView.Adapter<PedidosAdapter.PedidosViewHolder>() {
+class PedidosAdapter(val dataList:List<Pedidos>) : RecyclerView.Adapter<PedidosAdapter.PedidosViewHolder>() {
 
     inner class PedidosViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
         val pediText= itemView.iditem
@@ -16,14 +18,18 @@ class PedidosAdapter(val list:List<Pedidos>) : RecyclerView.Adapter<PedidosAdapt
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PedidosViewHolder {
+        val itemView= LayoutInflater.from(parent.context).inflate((R.layout.pedidos_item_list,parent,false))
+
+        return PedidosViewHolder(itemView)
+    }
+
+
+
+    override fun onBindViewHolder(holder: PedidosViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: PedidosViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        return dataList.size
     }
 }
